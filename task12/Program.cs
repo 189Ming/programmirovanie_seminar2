2334e4dd2e7dc26a1acdc21a6e11c8d9008d2a91
Dmitry <st1makk@gmail.com>
2023-04-18 22:39:13 +0700

    seminar2 done

--- task12/Program.cs
+++ task12/Program.cs
@@ -8,7 +8,7 @@
 
 Console.WriteLine("Введите первое число");
 int numberA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите первое число");
+Console.WriteLine("Введите второе число");
 int numberB = Convert.ToInt32(Console.ReadLine());
 int result = Remainder(numberA, numberB);
 
